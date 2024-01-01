89ce07b413c58d10897c66ca44257b703ad50f49
sonvice <wvmc_es@hotmail.com>
2024-01-01 17:57:29 +0100

    Add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import partytown from '@astrojs/partytown'
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://www.promptphi.com/',
-  integrations: [mdx(), sitemap(), react()],
-  adapter: netlify()
+  site: "http://www.promptphi.com/",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  adapter: netlify(),
 });
