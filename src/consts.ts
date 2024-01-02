b83cd712f7cc149ee98bf1d2193c8a828a25fdd6
sonvice <wvmc_es@hotmail.com>
2024-01-02 12:55:33 +0100

    Add meta tag social post

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,9 @@ export const SITE_TITLE = 'PromptPhi | Encuentra los Mejores Prompts';
 export const SITE_DESCRIPTION = 'Optimiza Tu Experiencia & Encuentra los Mejores Prompts para Desarrollar, Jugar & Potenciar tu Creatividad con la Inteligencia Artificial.';
 
 
-export const TOOLS_TITLE = 'IA Tools';
+export const BLOG_TITLE = 'PromptPhi | Blog';
+export const BLOG_DESCRIPTION = 'Explora las últimas novedades en inteligencia artificial en nuestro blog. Mantente al tanto de los avances, descubrimientos y tendencias más recientes en IA. Noticias actualizadas regularmente para mantenerte informado sobre el fascinante mundo de la inteligencia artificial.';
+
+
+export const TOOLS_TITLE = 'PromptPhi | IA Tools';
 export const TOOLS_DESCRIPTION = 'Explora la capacidad transformadora de las herramientas de inteligencia artificial, que van desde chatbots hasta algoritmos avanzados.'
